6c6b9498772c8ca895b454fd260256e2e0c1d2c1
Shukun Wang <86967307+KengWkeng@users.noreply.github.com>
2025-05-05 03:33:56 +0800

    Delete build directory

--- build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DataAcquisitionTest1_FILE C:/Users/80911/Documents/QT/DataAcquisitionTest1/build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/DataAcquisitionTest1.exe)
-set(__QT_DEPLOY_TARGET_DataAcquisitionTest1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DataAcquisitionTest1_RUNTIME_DLLS D:/Qt/6.8.1/msvc2022_64/bin/Qt6PrintSupportd.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6SerialBusd.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6SerialPortd.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6Networkd.dll;D:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
